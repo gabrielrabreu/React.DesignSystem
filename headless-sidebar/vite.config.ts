340d5a8b8a2d8b5a31902ffc2a534ee3ed6d37ab
gabrielrabreu <73451858+gabrielrabreu@users.noreply.github.com>
2024-10-19 18:10:28 -0300

    m

--- headless-sidebar/vite.config.ts
+++ headless-sidebar/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 
 import viteReact from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 import path from "path";
 
 export default defineConfig({
-  plugins: [viteReact(), TanStackRouterVite()],
+  plugins: [viteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
